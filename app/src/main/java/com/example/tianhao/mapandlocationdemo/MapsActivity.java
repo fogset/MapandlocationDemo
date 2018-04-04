4d7d4e78f9422f674731c17fc13dbe622381c8b3
Tianhao <zhangth1990@hotmail.com>
2018-04-03 23:26:56 -0600

    Log location info base on latitude and longtitude

--- app/src/main/java/com/example/tianhao/mapandlocationdemo/MapsActivity.java
+++ app/src/main/java/com/example/tianhao/mapandlocationdemo/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.tianhao.mapandlocationdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +14,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,6 +24,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -77,7 +85,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
 
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                    if(listAddresses != null && listAddresses.size() > 0){
+                        Log.i("PlaceInfo", listAddresses.get(0).toString());
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
